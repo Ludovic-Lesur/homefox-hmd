fd98f5b731c5eca877a0617654abf87ded5f9a94
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-09-21 12:40:19 +0200

    Add MCU pins mapping.

---
+++ drivers/peripherals/inc/mcu_mapping.h
@@ -0,0 +1,70 @@
+/*
+ * mcu_mapping.h
+ *
+ *  Created on: 21 sep. 2025
+ *      Author: Ludo
+ */
+
+#ifndef __MCU_MAPPING_H__
+#define __MCU_MAPPING_H__
+
+#include "adc.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "spi.h"
+#include "usart.h"
+
+/*** MCU MAPPING macros ***/
+
+#define ADC_CHANNEL_VBATT       ADC_CHANNEL_IN1
+
+#define I2C_INSTANCE_SENSORS    I2C_INSTANCE_I2C1
+
+#define SPI_INSTANCE_RADIO      SPI_INSTANCE_SPI1
+
+#define TIM_INSTANCE_MCU_API    TIM_INSTANCE_TIM2
+
+#define USART_INSTANCE_AT       USART_INSTANCE_USART2
+
+/*** MCU MAPPING structures ***/
+
+/*!******************************************************************
+ * \enum ADC_channel_index_t
+ * \brief ADC channels index.
+ *******************************************************************/
+typedef enum {
+    ADC_CHANNEL_INDEX_VBATT_MEASURE = 0,
+    ADC_CHANNEL_INDEX_LAST
+} ADC_channel_index_t;
+
+/*** MCU MAPPING global variables ***/
+
+// Analog inputs.
+extern const GPIO_pin_t GPIO_MNTR_EN;
+extern const ADC_gpio_t ADC_GPIO;
+// Button.
+extern const GPIO_pin_t GPIO_BUTTON;
+// Radio power control.
+extern const GPIO_pin_t GPIO_RF_POWER_ENABLE;
+extern const GPIO_pin_t GPIO_TCXO_POWER_ENABLE;
+// RF switch.
+extern const GPIO_pin_t GPIO_RF_SWITCH_CONTROL;
+// SX1261.
+extern const SPI_gpio_t SPI_GPIO_SX1261;
+extern const GPIO_pin_t GPIO_SX1261_CS;
+extern const GPIO_pin_t GPIO_SX1261_NRESET;
+extern const GPIO_pin_t GPIO_SX1261_BUSY;
+extern const GPIO_pin_t GPIO_SX1261_DIO1;
+extern const GPIO_pin_t GPIO_SX1261_DIO2;
+// Sensors.
+extern const GPIO_pin_t GPIO_SENSORS_POWER_ENABLE;
+extern const I2C_gpio_t I2C_GPIO_SENSORS;
+extern const GPIO_pin_t GPIO_SENSOR_IRQ;
+// AT interface.
+extern const USART_gpio_t USART_GPIO_AT;
+// RGB LED
+extern const GPIO_pin_t GPIO_LED_RED;
+extern const GPIO_pin_t GPIO_LED_GREEN;
+extern const GPIO_pin_t GPIO_LED_BLUE;
+
+#endif /* __MCU_MAPPING_H__ */
---
+++ drivers/peripherals/src/mcu_mapping.c
@@ -0,0 +1,61 @@
+/*
+ * gpio_mapping.c
+ *
+ *  Created on: 21 sep. 2025
+ *      Author: Ludo
+ */
+
+#include "mcu_mapping.h"
+
+#include "gpio.h"
+#include "gpio_registers.h"
+#include "i2c.h"
+#include "spi.h"
+#include "usart.h"
+
+/*** GPIO MAPPING local global variables ***/
+
+// Analog inputs.
+static const GPIO_pin_t GPIO_ADC_VBATT_MEASURE = { GPIOA, 0, 1, 0 };
+// Analog inputs list.
+static const GPIO_pin_t* const GPIO_ADC_PINS_LIST[ADC_CHANNEL_INDEX_LAST] = { &GPIO_ADC_VBATT_MEASURE };
+// I2C1.
+static const GPIO_pin_t GPIO_I2C1_SCL = { GPIOB, 1, 6, 1 };
+static const GPIO_pin_t GPIO_I2C1_SDA = { GPIOB, 1, 7, 1 };
+// SPI1.
+static const GPIO_pin_t GPIO_SPI1_SCK = { GPIOA, 0, 5, 0 };
+static const GPIO_pin_t GPIO_SPI1_MISO = { GPIOA, 0, 6, 0 };
+static const GPIO_pin_t GPIO_SPI1_MOSI = { GPIOA, 0, 7, 0 };
+// USART2.
+static const GPIO_pin_t GPIO_USART2_TX = { GPIOA, 0, 9, 4 };
+static const GPIO_pin_t GPIO_USART2_RX = { GPIOA, 0, 10, 4 };
+
+/*** GPIO MAPPING global variables ***/
+
+// Analog inputs.
+const GPIO_pin_t GPIO_MNTR_EN = { GPIOA, 0, 15, 0 };
+const ADC_gpio_t ADC_GPIO = { (const GPIO_pin_t**) &GPIO_ADC_PINS_LIST, ADC_CHANNEL_INDEX_LAST };
+// Button.
+const GPIO_pin_t GPIO_BUTTON = { GPIOA, 0, 2, 0 };
+// Radio power control.
+const GPIO_pin_t GPIO_RF_POWER_ENABLE = { GPIOA, 0, 12, 0 };
+const GPIO_pin_t GPIO_TCXO_POWER_ENABLE = { GPIOB, 1, 2, 0 };
+// RF switch.
+const GPIO_pin_t GPIO_RF_SWITCH_CONTROL = { GPIOA, 0, 3, 0 };
+// S2LP.
+const SPI_gpio_t SPI_GPIO_SX1261 = { &GPIO_SPI1_SCK, &GPIO_SPI1_MOSI, &GPIO_SPI1_MISO };
+const GPIO_pin_t GPIO_SX1261_CS = { GPIOA, 0, 4, 0 };
+const GPIO_pin_t GPIO_SX1261_NRESET = { GPIOB, 1, 0, 0 };
+const GPIO_pin_t GPIO_SX1261_BUSY = { GPIOB, 1, 1, 0 };
+const GPIO_pin_t GPIO_SX1261_DIO1 = { GPIOA, 0, 8, 0 };
+const GPIO_pin_t GPIO_SX1261_DIO2 = { GPIOA, 0, 11, 0 };
+// Sensors.
+const GPIO_pin_t GPIO_SENSORS_POWER_ENABLE = { GPIOB, 1, 3, 0 };
+const I2C_gpio_t I2C_GPIO_SENSORS = { &GPIO_I2C1_SCL, &GPIO_I2C1_SDA };
+const GPIO_pin_t GPIO_SENSOR_IRQ = { GPIOA, 0, 0, 0 };
+// AT interface.
+const USART_gpio_t USART_GPIO_AT = { &GPIO_USART2_TX, &GPIO_USART2_RX };
+// RGB LED
+const GPIO_pin_t GPIO_LED_RED = { GPIOB, 1, 4, 0 };
+const GPIO_pin_t GPIO_LED_GREEN = { GPIOB, 1, 8, 0 };
+const GPIO_pin_t GPIO_LED_BLUE = { GPIOB, 1, 5, 0 };
