cd230884acac694f17d6eb47af39acef4bf5564d
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-10-07 18:56:02 +0200

    Fix button driver error macros.

--- drivers/components/inc/button.h
+++ drivers/components/inc/button.h
@@ -46,12 +46,12 @@ BUTTON_status_t BUTTON_init(EXTI_gpio_irq_cb_t irq_callback);
 BUTTON_status_t BUTTON_de_init(void);
 
 /*******************************************************************/
-#define BUTTON_exit_error(base) { ERROR_check_exit(led_status, BUTTON_SUCCESS, base) }
+#define BUTTON_exit_error(base) { ERROR_check_exit(button_status, BUTTON_SUCCESS, base) }
 
 /*******************************************************************/
-#define BUTTON_stack_error(base) { ERROR_check_stack(led_status, BUTTON_SUCCESS, base) }
+#define BUTTON_stack_error(base) { ERROR_check_stack(button_status, BUTTON_SUCCESS, base) }
 
 /*******************************************************************/
-#define BUTTON_stack_exit_error(base, code) { ERROR_check_stack_exit(led_status, BUTTON_SUCCESS, base, code) }
+#define BUTTON_stack_exit_error(base, code) { ERROR_check_stack_exit(button_status, BUTTON_SUCCESS, base, code) }
 
 #endif /* __BUTTON_H__ */
